3634b82f0e1aaa8380c59dcaf5292bd5d70e32c9
KASYMJAN RYZAKULYYEV <macpro2020@MacBook-Pro.local>
2021-01-10 15:33:45 -0800

    Created and array blog post

--- src/App.js
+++ src/App.js
@@ -3,12 +3,33 @@ import './App.css';
 
 // import {BlogPost} from './BlogPost'
 // import Greeting from './Greeting'
+const posts =[
+  {title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+  body:'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto'},
+  {title: 'qui est esse',
+  body:'est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla'},
+  {title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut',
+  body:'et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut'},
+  {title: 'eum et est occaecati',
+  body:'ullam et saepe reiciendis voluptatem adipisci sit amet autem assumenda provident rerum culpa quis hic commodi nesciunt rem tenetur doloremque ipsam iure quis sunt voluptatem rerum illo velit'},
+  {title: 'nesciunt quas odio',
+  body:'repudiandae veniam quaerat sunt sed alias aut fugiat sit autem sed est voluptatem omnis possimus esse voluptatibus quis est aut tenetur dolor neque'}
+]
 function App() {
   return (
     <div>
       <h1 className="text-center">Welcome to my Blog</h1>
       {/* <BlogPost isAdmin={false}/>
       <Greeting myName='Kasim' isAdmin={false}/> */}
+      <section>
+        {posts.map((post)=>{
+        return (<article>
+           <h2>{post.title}</h2>
+           <p>{post.body}</p>
+         </article>)
+        })}
+       
+    </section>
     </div>
  
   );
